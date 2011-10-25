e3df8894521d4eab561f0c4f677172814cef692e
Yamamoto Kazuhisa <ak.hisashi@gmail.com>
2011-10-25 15:29:34 +0900

    add spec.

--- Gemfile
+++ Gemfile
@@ -2,6 +2,10 @@ source "http://rubygems.org"
 # Add dependencies required to use your gem here.
 # Example:
 #   gem "activesupport", ">= 2.3.5"
+rails_version = '~> 3.1.1'
+
+gem 'activerecord',   rails_version
+gem 'actionpack',     rails_version
 
 # Add dependencies to develop your gem here.
 # Include everything needed to run rake, tests, features, etc.
@@ -10,4 +14,8 @@ group :development do
   gem "bundler", "~> 1.0.0"
   gem "jeweler", "~> 1.6.4"
   gem "rcov", ">= 0"
+end
+
+group :test do
+  gem 'rr'
 end
--- spec/need_label_spec.rb
+++ spec/need_label_spec.rb
@@ -1,7 +1,21 @@
+require 'spec_helper'
+require 'need_label'
+require 'active_support/rescuable' # needed for Ruby 1.9.1
+require 'action_controller'
+
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "NeedLabel" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+Routes = ActionDispatch::Routing::RouteSet.new
+
+Routes.draw do
+  match ':controller(/:action(/:id(.:format)))'
+end
+
+describe "exists need_label" do
+  class MyModel
+    extend ActionView::Helpers::FormHelper
+  end
+  it "respond to need_label" do
+    MyModel.respond_to?("need_label").should be_true
   end
 end
